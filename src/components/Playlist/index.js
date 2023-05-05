818b6211eb9825acde60ca896de1775fa0862b2d
Cu Sinh <sinhnguyen2k2@gmail.com>
2023-05-05 16:25:25 +0700

    updata playlist

--- src/components/Playlist/index.js
+++ src/components/Playlist/index.js
@@ -5,6 +5,9 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 import Header from '~/components/Header';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay, faEllipsis } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -12,6 +15,13 @@ function Playlist() {
     const location = useLocation();
     const { from } = location.state || {};
     const [dataPlaylist, setdataPlaylist] = useState({});
+
+    const convertTime = (duration) => {
+        let resuilt = (duration / 1000 / 60).toFixed(2);
+        let numArr = resuilt.toString().split('.');
+
+        return `${Math.floor(resuilt)}:${numArr[1]}`;
+    };
 
     useEffect(() => {
         const callApiPlaylist = async () => {
@@ -26,12 +36,41 @@ function Playlist() {
         callApiPlaylist();
     }, [from]);
 
-    const render = (data) => {};
+    const render = (data) => {
+        return (
+            data &&
+            data.items.length > 0 &&
+            data.items.map((item, index) => {
+                return (
+                    <div className={cx('song')} key={index}>
+                        <div className={cx('col1')}>{index + 1}</div>
+                        <div className={cx('col2', 'title')}>
+                            <img alt="pic" className={cx('title-image')} src={item.track.album.images[0].url}></img>
+                            <div className={cx('title-about')}>
+                                <p className={cx('about-name')}>{item.track.name}</p>
+                                <p className={cx('about-artist')}>
+                                    {item.track.artists
+                                        .map((value) => {
+                                            return value.name;
+                                        })
+                                        .join(', ')}
+                                </p>
+                            </div>
+                        </div>
+                        <div className={cx('col3')}>{item.track.album.name}</div>
+                        <div className={cx('col4')}>{item.added_at.substring(0, 10)}</div>
+                        <div className={cx('col5')}>{convertTime(item.track.duration_ms)}</div>
+                    </div>
+                );
+            })
+        );
+    };
 
     return (
         <React.Fragment>
             {console.log(dataPlaylist)}
             <Header></Header>
+
             <div className={cx('wrapper')}>
                 <div className={cx('banner')}>
                     <img
@@ -45,7 +84,23 @@ function Playlist() {
                         <p className={cx('discription')}>{dataPlaylist.description}</p>
                     </div>
                 </div>
-                <div className={cx('songs')}>{render(dataPlaylist)}</div>
+                <div className={cx('controls')}>
+                    <FontAwesomeIcon icon={faCirclePlay} className={cx('icon')}></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faHeart} className={cx('icon')}></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faEllipsis} className={cx('icon')}></FontAwesomeIcon>
+                </div>
+
+                <div className={cx('songs')}>
+                    <div className={cx('genre')}>
+                        <div className={cx('col1')}>#</div>
+                        <div className={cx('col2')}>Title</div>
+                        <div className={cx('col3')}>Album</div>
+                        <div className={cx('col4')}>Add day</div>
+                        <div className={cx('col5')}>Time</div>
+                    </div>
+
+                    {render(dataPlaylist.tracks)}
+                </div>
             </div>
         </React.Fragment>
     );
